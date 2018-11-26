a13cdb2539d0948e413d6d09507ebfc608c4a636
Matan Tsuberi <tsuberim@gmail.com>
2018-11-26 18:08:56 +0200

    initial

---
+++ crash.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3');
+
+async function main() {
+	const web3 = new Web3('http://localhost:8545');
+	const account = web3.eth.accounts.privateKeyToAccount(
+		'0x8d4408014d165ec69d8cc9f091d8f4578ac5564f376f21887e98a6d33a6e3549'
+	);
+	web3.eth.accounts.wallet.add(account);
+	const txCount = await web3.eth.getTransactionCount(account.address);
+	console.log(txCount);
+	const tx = await web3.eth.sendTransaction({
+		from: account.address,
+		to: '0x9c7f9f45a22ad3d667a5439f72b563df3aa70aae',
+		value: web3.utils.toWei('1', 'ether'),
+		gas: 8000000,
+	});
+	console.log(tx);
+}
+
+main()
+	.then(console.log)
+	.catch(console.error);
